98e4d95005156ba6270a1a6889801a144ecdf65a
Long <longmatic1.0@gmail.com>
2025-08-11 14:18:21 +0700

    splash screen

---
+++ app/src/main/java/com/example/shoponlineapp/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.shoponlineapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shoponlineapp.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            startBtn.setOnClickListener{
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            }
+        }
+    }
+}
