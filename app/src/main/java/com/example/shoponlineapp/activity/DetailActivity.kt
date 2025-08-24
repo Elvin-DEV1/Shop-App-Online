ef9d316f7c8e8ff7847b3477a0c4121d8f79bea4
Long <longmatic1.0@gmail.com>
2025-08-24 15:19:50 +0700

    item detail xml

---
+++ app/src/main/java/com/example/shoponlineapp/activity/DetailActivity.kt
@@ -0,0 +1,20 @@
+package com.example.shoponlineapp.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.shoponlineapp.R
+import com.example.shoponlineapp.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail)
+
+    }
+}
